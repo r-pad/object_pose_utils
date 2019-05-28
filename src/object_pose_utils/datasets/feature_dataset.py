1fce15cd66c17b81a3643a15fea105e8ec9d1b6b
Brian Okorn <bokorn@andrew.cmu.edu>
2019-05-28 15:27:38 -0400

    Brokout data processesing so it can be called by preprocessors

--- src/object_pose_utils/datasets/feature_dataset.py
+++ src/object_pose_utils/datasets/feature_dataset.py
@@ -5,42 +5,58 @@ Created on Tue May 22 22:38:19 2018
 @author: bokorn
 """
 import numpy as np
-import numpy.ma as ma
-from PIL import Image
-import scipy.io as scio
+import torch 
 import os
-import pickle
 
 from object_pose_utils.datasets.pose_dataset import PoseDataError
 
-from object_pose_utils.datasets.uniform_dataset import UniformYCBDataset
+from object_pose_utils.datasets.uniform_ycb_dataset import UniformYCBDataset
 from object_pose_utils.datasets.ycb_dataset import YcbDataset as YCBDataset
 
 class FeatureDataset(YCBDataset):
-    def __init__(self, feature_path, *args, **kwargs):
+    def __init__(self, feature_root, *args, **kwargs):
         super(FeatureDataset, self).__init__(output_data = [], 
-                                                    *args, **kwargs)
+                                             *args, **kwargs)
 
-        self.feature_path = feature_path
+        self.feature_root = feature_root
 
     def __getitem__(self, index):
-        obj = torch.LongTensor([self.object_label])
-        data = np.load(self.feature_path + self.getPath(index))
+        try:
+            meta_data = self.getMetaData(index)
+            obj = meta_data['object_label']
+            data = np.load(os.path.join(self.feature_root, 'data', self.getPath(index)) + 
+                    '_{}_feat.npz'.format(self.classes[obj]))
+        except (IOError, PoseDataError)  as e:
+            print('Exception on index {}: {}'.format(index, e))
+            if(self.resample_on_error):
+                return self.__getitem__(np.random.randint(0, len(self)))
+            else:
+                return [], [], []
+
+        obj = torch.LongTensor([obj])
         quat = torch.from_numpy(data['quat'].astype(np.float32))
         feat = torch.from_numpy(data['feat'].astype(np.float32))
         return obj, feat, quat
 
 class UniformFeatureDataset(UniformYCBDataset):
-    def __init__(self, feature_path, *args, **kwargs):
+    def __init__(self, feature_root, *args, **kwargs):
         super(UniformFeatureDataset, self).__init__(output_data = [], 
                                                     *args, **kwargs)
 
-        self.feature_path = feature_path
+        self.feature_root = feature_root
 
     def __getitem__(self, index):
-        meta_data = self.getMetaData(index)
-        obj = torch.LongTensor([meta_data['object_label']])
-        data = np.load(self.feature_path + self.getPath(index))
+        try:
+            data = np.load(os.path.join(self.feature_root, self.getPath(index)) + 
+                    '_{}_feat.npz'.format(self.classes[self.object_label]))
+        except (IOError, PoseDataError)  as e:
+            print('Exception on index {}: {}'.format(index, e))
+            if(self.resample_on_error):
+                return self.__getitem__(np.random.randint(0, len(self)))
+            else:
+                return [], [], []
+
+        obj = torch.LongTensor([self.object_label])
         quat = torch.from_numpy(data['quat'].astype(np.float32))
         feat = torch.from_numpy(data['feat'].astype(np.float32))
         return obj, feat, quat
--- src/object_pose_utils/datasets/pose_dataset.py
+++ src/object_pose_utils/datasets/pose_dataset.py
@@ -105,14 +105,93 @@ CAMERA_MATRIX_OUTPUTS = set([OutputTypes.DEPTH_POINTS,
                             OutputTypes.DEPTH_POINTS_MASKED_AND_INDEXES]
                             )
 
+
+def processImage(img, meta_data, output_type, 
+                 remove_mask = True,
+                 background_fill = 255., 
+                 boarder_width = 0.0,
+                 ):
+    if(output_type in MASK_OUTPUTS):
+        img = np.concatenate([img, meta_data['mask']], axis=2)
+        if(self.background_fill is not None):
+            image = transparentOverlay(img, background_fill, remove_mask=remove_mask)
+    if(output_type in BBOX_OUTPUTS):
+        img, _ = cropBBox(img, meta_data['bbox'], boarder_width)
+    return torch.from_numpy(np.transpose(img, (2,0,1)).astype(np.float32))
+
+def processDepthImage(depth, meta_data, output_type, 
+                      num_points = 1000,
+                      boarder_width = 0.0,
+                      ):
+    if(output_type in MASK_OUTPUTS):
+        depth = ma.masked_array(depth, ~np.bitwise_and(meta_data['mask'], depth != 0))
+
+    if(output_type in BBOX_OUTPUTS):
+        depth, corner = cropBBox(depth, meta_data['bbox'], boarder_width)
+    else:
+        corner = (0,0)
+
+    if(output_type in DEPTH_POINT_OUTPUTS):
+        y_size, x_size = depth.shape[:2]
+        ymap, xmap = np.meshgrid(np.arange(x_size), np.arange(y_size))
+        ymap += corner[0]
+        xmap += corner[1]
+        choose = depth.flatten().nonzero()[0]
+        if len(choose) > num_points:
+            c_mask = np.zeros(len(choose), dtype=int)
+            c_mask[:self.num_points] = 1
+            np.random.shuffle(c_mask)
+            choose = choose[c_mask.nonzero()]
+        elif len(choose) > 0:
+            choose = np.pad(choose, (0, num_points - len(choose)), 'wrap')
+        else:
+            raise PoseDataError('No points in mask')
+        
+        depth_choose = depth.flatten()[choose][:, np.newaxis].astype(np.float32)
+        xmap_choose = xmap.flatten()[choose][:, np.newaxis].astype(np.float32)
+        ymap_choose = ymap.flatten()[choose][:, np.newaxis].astype(np.float32)
+        choose = np.array([choose])
+
+        z = depth_choose / meta_data['camera_scale']
+        x = (ymap_choose - meta_data['camera_cx']) * z / meta_data['camera_fx']
+        y = (xmap_choose - meta_data['camera_cy']) * z / meta_data['camera_fy']
+        cloud = np.concatenate((x, y, z), axis=1)
+        cloud = torch.from_numpy(cloud.astype(np.float32))
+        if(output_type in POINT_INDEX_OUTPUTS):
+            return [cloud, torch.LongTensor(choose.astype(np.int32))]
+        else:
+            return cloud
+    
+    return torch.from_numpy(depth.astype(np.float32))
+
+def processModelPoints(points, meta_data, output_type):
+    if(output_type is OutputTypes.MODEL_POINTS_TRANSFORMED):
+        points = points
+        R = meta_data['transform_mat'][:3,:3]
+        t = meta_data['transform_mat'][:3,3]
+        points = np.add(np.dot(points, R.T), t)
+    return torch.from_numpy(points.astype(np.float32))
+
+def processTransform(meta_data, output_type): 
+    if(output_type is OutputTypes.ROTATION_MATRIX):
+        return torch.from_numpy(meta_data['transform_mat'][:3,:3].astype(np.float32))
+    if(output_type is OutputTypes.QUATERNION):
+        R = np.eye(4)
+        R[:3,:3] = meta_data['transform_mat'][:3,:3]
+        return torch.from_numpy(quaternion_from_matrix(R).astype(np.float32))
+    if(output_type is OutputTypes.TRANSLATION):
+        return torch.from_numpy(meta_data['transform_mat'][:3,3].astype(np.float32))
+    return torch.from_numpy(meta_data['transform_mat'].astype(np.float32))
+
+
 class PoseDataset(Dataset):
     def __init__(self, 
-                 image_size,
                  output_data = [
                                 OutputTypes.IMAGE_CROPPED,
                                 #OutputTypes.QUATERNION,
                                ],
                  preprocessor = None,
+                 image_size = [640, 480],
                  num_points = -1,
                  resample_on_error = False,
                  *args, **kwargs):
@@ -151,17 +230,22 @@ class PoseDataset(Dataset):
                 if(output_type in IMAGE_OUTPUTS):
                     if('img' not in locals()):
                         img = self.getImage(index)
-                    outputs.append(self.processImage(img.copy(), meta_data, output_type))
+                    outputs.append(processImage(img.copy(), meta_data, output_type,
+                                                remove_mask = self.remove_mask,
+                                                background_fill = self.background_fill,
+                                                boarder_width = self.boarder_width))
                 elif(output_type in DEPTH_OUTPUTS):
                     if('depth' not in locals()):
                         depth = self.getDepthImage(index)
-                    outputs.extend(self.processDepthImage(depth.copy(), meta_data, output_type))
+                    outputs.extend(processDepthImage(depth.copy(), meta_data, output_type,
+                                                     num_points = self.num_points,
+                                                     boarder_width = self.boarder_width))
                 elif(output_type in MODEL_POINT_OUTPUTS):
                     if('points' not in locals()):
                         points = self.getModelPoints(meta_data['object_label'])
-                    outputs.append(self.processModelPoints(points.copy(), meta_data, output_type))
+                    outputs.append(processModelPoints(points.copy(), meta_data, output_type))
                 elif(output_type in TRANSFORM_OUTPUTS):
-                    outputs.append(self.processTransform(meta_data, output_type))
+                    outputs.append(processTransform(meta_data, output_type))
                 elif(output_type is OutputTypes.OBJECT_LABEL):
                     outputs.append(torch.LongTensor([meta_data['object_label']]))
                 elif(output_type is OutputTypes.MASK):
@@ -181,77 +265,6 @@ class PoseDataset(Dataset):
             outputs = self.preprocessor(outputs, meta_data, self.output_data_buffered)
             
         return tuple(outputs)
-
-    def processImage(self, img, meta_data, output_type):
-        if(output_type in MASK_OUTPUTS):
-            img = np.concatenate([img, meta_data['mask']], axis=2)
-            if(self.background_fill is not None):
-                image = transparentOverlay(img, self.background_fill, remove_mask=self.remove_mask)
-        if(output_type in BBOX_OUTPUTS):
-            img, _ = cropBBox(img, meta_data['bbox'], self.boarder_width)
-        return torch.from_numpy(np.transpose(img, (2,0,1)).astype(np.float32))
-
-    def processDepthImage(self, depth, meta_data, output_type):
-        if(output_type in MASK_OUTPUTS):
-            depth = ma.masked_array(depth, ~np.bitwise_and(meta_data['mask'], depth != 0))
-
-        if(output_type in BBOX_OUTPUTS):
-            depth, corner = cropBBox(depth, meta_data['bbox'], self.boarder_width)
-        else:
-            corner = (0,0)
-
-        if(output_type in DEPTH_POINT_OUTPUTS):
-            y_size, x_size = depth.shape[:2]
-            ymap, xmap = np.meshgrid(np.arange(x_size), np.arange(y_size))
-            ymap += corner[0]
-            xmap += corner[1]
-            choose = depth.flatten().nonzero()[0]
-            if len(choose) > self.num_points:
-                c_mask = np.zeros(len(choose), dtype=int)
-                c_mask[:self.num_points] = 1
-                np.random.shuffle(c_mask)
-                choose = choose[c_mask.nonzero()]
-            elif len(choose) > 0:
-                choose = np.pad(choose, (0, self.num_points - len(choose)), 'wrap')
-            else:
-                raise PoseDataError('No points in mask')
-            
-            depth_choose = depth.flatten()[choose][:, np.newaxis].astype(np.float32)
-            xmap_choose = xmap.flatten()[choose][:, np.newaxis].astype(np.float32)
-            ymap_choose = ymap.flatten()[choose][:, np.newaxis].astype(np.float32)
-            choose = np.array([choose])
-
-            z = depth_choose / meta_data['camera_scale']
-            x = (ymap_choose - meta_data['camera_cx']) * z / meta_data['camera_fx']
-            y = (xmap_choose - meta_data['camera_cy']) * z / meta_data['camera_fy']
-            cloud = np.concatenate((x, y, z), axis=1)
-            cloud = torch.from_numpy(cloud.astype(np.float32))
-            if(output_type in POINT_INDEX_OUTPUTS):
-                return [cloud, torch.LongTensor(choose.astype(np.int32))]
-            else:
-                return cloud
-        
-        return torch.from_numpy(depth.astype(np.float32))
-
-    def processModelPoints(self, points, meta_data, output_type):
-        if(output_type is OutputTypes.MODEL_POINTS_TRANSFORMED):
-            points = points
-            R = meta_data['transform_mat'][:3,:3]
-            t = meta_data['transform_mat'][:3,3]
-            points = np.add(np.dot(points, R.T), t)
-        return torch.from_numpy(points.astype(np.float32))
-
-    def processTransform(self, meta_data, output_type): 
-        if(output_type is OutputTypes.ROTATION_MATRIX):
-            return torch.from_numpy(meta_data['transform_mat'][:3,:3].astype(np.float32))
-        if(output_type is OutputTypes.QUATERNION):
-            R = np.eye(4)
-            R[:3,:3] = meta_data['transform_mat'][:3,:3]
-            return torch.from_numpy(quaternion_from_matrix(R).astype(np.float32))
-        if(output_type is OutputTypes.TRANSLATION):
-            return torch.from_numpy(meta_data['transform_mat'][:3,3].astype(np.float32))
-        return torch.from_numpy(meta_data['transform_mat'].astype(np.float32))
-
     def getDepthImage(self, index):
         raise NotImplementedError('getDepthImage must be implemented by child classes')
     def getImage(self, index):
