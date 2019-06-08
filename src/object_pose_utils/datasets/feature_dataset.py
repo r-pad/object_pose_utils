9426cacbc4860783452736978f53311681a1451e
Brian Okorn <brian.e.okorn@gmail.com>
2019-06-08 09:30:15 -0500

    Added augmentation number to feature data loader

--- src/object_pose_utils/datasets/feature_dataset.py
+++ src/object_pose_utils/datasets/feature_dataset.py
@@ -14,18 +14,24 @@ from object_pose_utils.datasets.uniform_ycb_dataset import UniformYCBDataset
 from object_pose_utils.datasets.ycb_dataset import YcbDataset as YCBDataset
 
 class FeatureDataset(YCBDataset):
-    def __init__(self, feature_root, *args, **kwargs):
+    def __init__(self, feature_root, num_augs = 0, *args, **kwargs):
         super(FeatureDataset, self).__init__(output_data = [], 
                                              *args, **kwargs)
 
         self.feature_root = feature_root
+        self num_augs = num_augs
 
     def __getitem__(self, index):
         try:
             meta_data = self.getMetaData(index)
             obj = meta_data['object_label']
-            data = np.load(os.path.join(self.feature_root, 'data', self.getPath(index)) + 
-                    '_{}_feat.npz'.format(self.classes[obj]))
+            if(self.num_augs > 0):
+                aug_idx = np.random.randint(self.num_augs)
+                data = np.load(os.path.join(self.feature_root, 'data', self.getPath(index)) + 
+                        '_{}_{}_feat.npz'.format(self.classes[obj], aug_idx))
+            else:
+                data = np.load(os.path.join(self.feature_root, 'data', self.getPath(index)) + 
+                        '_{}_{}_feat.npz'.format(self.classes[obj]))
         except (IOError, PoseDataError)  as e:
             print('Exception on index {}: {}'.format(index, e))
             if(self.resample_on_error):
@@ -39,16 +45,22 @@ class FeatureDataset(YCBDataset):
         return obj, feat, quat
 
 class UniformFeatureDataset(UniformYCBDataset):
-    def __init__(self, feature_root, *args, **kwargs):
+    def __init__(self, feature_root, num_augs = 0, *args, **kwargs):
         super(UniformFeatureDataset, self).__init__(output_data = [], 
                                                     *args, **kwargs)
 
         self.feature_root = feature_root
+        self num_augs = num_augs
 
     def __getitem__(self, index):
         try:
-            data = np.load(os.path.join(self.feature_root, self.getPath(index)) + 
-                    '_{}_feat.npz'.format(self.classes[self.object_label]))
+            if(self.num_augs > 0):
+                aug_idx = np.random.randint(self.num_augs)
+                data = np.load(os.path.join(self.feature_root, self.getPath(index)) + 
+                        '_{}_feat.npz'.format(self.classes[self.object_label], aug_idx))
+            else:
+                data = np.load(os.path.join(self.feature_root, self.getPath(index)) + 
+                        '_{}_feat.npz'.format(self.classes[self.object_label]))
         except (IOError, PoseDataError)  as e:
             print('Exception on index {}: {}'.format(index, e))
             if(self.resample_on_error):
